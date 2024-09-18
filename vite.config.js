e654e99ad3176bf91ef05646f5380a183ec784e6
s1mple0315 <casscassovich@gmail.com>
2024-09-18 17:35:51 +0500

    implemented port forwarding

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://194.87.207.151:8000', // API server
