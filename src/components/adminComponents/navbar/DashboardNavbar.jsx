0a07dbcd7675855833921d6c1d89886cedea2d18
s1mple0315 <145556641+s1mple0315@users.noreply.github.com>
2024-10-02 16:08:56 +0500

    admin panel in development....

--- src/components/adminComponents/navbar/DashboardNavbar.jsx
+++ src/components/adminComponents/navbar/DashboardNavbar.jsx
@@ -3,13 +3,29 @@ import styles from "./DashboardNavbar.module.css";
 const DashboardNavbar = () => {
   return (
     <nav
-      className={`${styles.navbarContainer} d-flex flex-coluZmn postition-absolute`}
-    > <ul>
-        <li></li>
-        <li>1</li>
-        <li>1</li>
-        <li>1</li>
-    </ul>
+      className={`${styles.navbarContainer} d-flex flex-column position-absolute`}
+    >
+      <div className={`${styles.percentageContainer}`}>
+        <p>Сколько фотографий в меню</p>
+        <p>Хороший показатель: от 90%</p>
+        <strong>Сейчас: 84%</strong>
+      </div>
+      <div>
+        <div className={`${styles.titlePhoto} d-flex flex-column gap-2`}>
+          <strong>Титульное фото</strong>
+          <button>Редактировать</button>
+        </div>
+        <div className={`${styles.importButtonContainer} d-flex`}>
+          <button>Импорт меню</button>
+        </div>
+        <div className={`${styles.stopList}`}>
+          <strong>Стоп-лист</strong>
+          <p>1 позиция</p>
+        </div>
+      </div>
+      <div className={`${styles} d-flex `}>
+        <span><p>Категории</p> 9</span>
+      </div>
     </nav>
   );
 };
